19e86aec22feb0a9536caa2cb05138ded6caf26e
Josh Quintus <josh.quintus@gmaillcom>
2026-04-21 01:59:55 +0000

    Add the end cap to the model

--- cad/engine.py
+++ cad/engine.py
@@ -37,35 +37,58 @@ def build(config):
             f"top_hole_height must be between 0 and total height ({total_height}), got {top_hole_height}"
         )
 
-    result = cq.Workplane("XY").circle(cylinder_outside_diameter / 2).extrude(cylinder_height)
-
-    if frustum_height > 0:
-        frustum = (
-            cq.Workplane("XY")
-            .workplane(offset=cylinder_height)
-            .circle(frustum_bottom_diameter / 2)
-            .workplane(offset=frustum_height)
-            .circle(frustum_top_diameter / 2)
-            .loft(combine=True)
-        )
-        result = result.union(frustum)
-
     bottom_hole_height = total_height - top_hole_height
-    cut_solid = None
+    end_cap_thickness = 2
+    fillet_radius = 1
 
-    if bottom_hole_height > 0:
-        cut_solid = cq.Workplane("XY").circle(bottom_hole_diameter / 2).extrude(bottom_hole_height)
+    def origin ():
+        return cq.Workplane("XY")
 
-    if top_hole_height > 0:
-        top_cut = (
-            cq.Workplane("XY")
-            .workplane(offset=bottom_hole_height)
-            .circle(top_hole_diameter / 2)
-            .extrude(top_hole_height)
+    nose_cone = (
+        origin()
+        .circle(cylinder_outside_diameter / 2)
+        .extrude(cylinder_height)
+        .faces(">Z")
+        .workplane()
+        .circle(frustum_bottom_diameter / 2)
+        .workplane(offset=frustum_height)
+        .circle(frustum_top_diameter / 2)
+        .loft(combine=True)
+    )
+
+    cut_solid = (
+        origin()
+        .circle(bottom_hole_diameter/ 2)
+        .extrude(bottom_hole_height)
+        .faces(">Z")
+        .circle(top_hole_diameter / 2)
+        .extrude(top_hole_height)
         )
-        cut_solid = top_cut if cut_solid is None else cut_solid.union(top_cut)
 
-    if cut_solid is not None:
-        result = result.cut(cut_solid)
+    nose_cone = (
+        nose_cone
+        .edges(">Z or <Z")
+        .fillet(fillet_radius)
+    )
+
+    nose_cone = nose_cone.cut(cut_solid).translate((20, 0, 0))
+
+
+    end_cap = (
+        origin()
+        .circle(frustum_bottom_diameter / 2)
+        .extrude(end_cap_thickness)
+        .faces(">Z")
+        .circle(cylinder_outside_diameter / 2)
+        .extrude(cylinder_height)
+    )
+
+    end_cap = (
+        end_cap
+        .edges(">Z or <Z")
+        .fillet(fillet_radius)
+    )
+
+    result = nose_cone.union(end_cap)
 
     return result
